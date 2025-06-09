bc408298dc5630427b1828c21a43a2add4ec270b
Farindra <farindrabahadurbhandari@gmail.com>
2025-06-09 12:46:43 +0545

    Add Project model with columns for title, slug, description, thumbnailUrl, gallery, technologies, category, tags, websiteUrl, githubUrl, startDate, and endDate

---
+++ src/models/project.model.ts
@@ -0,0 +1,91 @@
+import { Table, Column, Model, DataType } from "sequelize-typescript";
+
+@Table({
+  tableName: "projects",
+  modelName: "Project",
+  timestamps: true, //
+})
+class Project extends Model {
+  // define title column
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare title: string;
+
+  // define slug column
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare slug: string;
+
+  // define description column
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare description: string;
+
+  // define thumbnailUrl colum
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare thumbnailUrl: string;
+
+  // define gallery column
+  @Column({
+    type: DataType.JSON,
+    allowNull: true,
+  })
+  declare gallery: string[];
+
+  // define technologies column
+  @Column({
+    type: DataType.JSON,
+    allowNull: false,
+  })
+  declare technologies: string[];
+
+  // define category column
+  @Column({
+    type: DataType.JSON,
+    allowNull: false,
+  })
+  declare category: string[];
+
+  // define tag column
+  @Column({
+    type: DataType.JSON,
+    allowNull: false,
+  })
+  declare tags: string[];
+
+  // define website Url column
+  @Column({
+    type: DataType.STRING,
+  })
+  declare websiteUrl: string;
+
+  // define github Url column
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare githubUrl: string;
+
+  // define project startDate column
+  @Column({
+    type: DataType.DATE,
+  })
+  declare startDate: Date;
+
+  // define project endDate column
+  @Column({
+    type: DataType.DATE,
+  })
+  declare endDate: Date;
+}
+
+export default Project;
