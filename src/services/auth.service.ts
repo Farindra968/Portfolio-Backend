462766c5363139d658d9a7ff1464c744ae90e7b1
Farindra <farindrabahadurbhandari@gmail.com>
2025-06-15 13:27:59 +0545

    Add update password functionality with validation and error handling

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,48 +1,81 @@
-import bcrypt from "bcrypt"
-import User from "../models/user.model"
+import bcrypt from "bcrypt";
+import User from "../models/user.model";
 
-const authRegister = async(data:any)=>{
-
-    const existingUser = await User.findOne({where:{email: data.email}})
-
-    if(existingUser) {
-        throw {status: 409, message:"User already exist"}
-    }
-
-    const hashPassword = await bcrypt.hash(data.password, 12)
-    const register = await User.create({
-        username: data.username,
-        password: hashPassword,
-        email: data.email,
-        
-    })
-
-    if(!register) {
-        throw {status: 400, message: 'Registeration failed'}
-    }
-
-    return register;
+interface IUser {
+  username: string;
+  password: string;
+  currentpassword: string;
+  email: string;
+  role?: "superAdmin" | "admin" | "User";
+  profilePicture?: string;
 }
 
+const authRegister = async (data: IUser) => {
+  const existingUser = await User.findOne({ where: { email: data.email } });
 
-const authLogin = async(data:any)=> {
+  if (existingUser) {
+    throw { status: 409, message: "User already exist" };
+  }
 
-    // 1. find email and password
-    const user = await User.findOne({where: {email: data.email}})
-    // if email not found in database
-    if(!user) {
-        throw {status:400, message: "User not found"}
-    }
-    
-    // 2. compare user password with database encrypt password
-    const comparePassword = await bcrypt.compare(data.password, user.password)
-    // if user password and database password did not match
-    if(!comparePassword) {
-        throw {status:400, message: "Invalid email or password"}
-    }
+  const hashPassword = await bcrypt.hash(data.password, 12);
+  const register = await User.create({
+    username: data.username,
+    password: hashPassword,
+    email: data.email,
+    role: data.role,
+    profilePicture: data.profilePicture,
+  });
 
-    return user;
+  if (!register) {
+    throw { status: 400, message: "Registeration failed" };
+  }
 
-}
+  return register;
+};
 
-export default {authRegister, authLogin}
+const authLogin = async (data: IUser) => {
+  // 1. find email and password
+  const user = await User.findOne({ where: { email: data.email } });
+  // if email not found in database
+  if (!user) {
+    throw { status: 400, message: "User not found" };
+  }
+
+  // 2. compare user password with database encrypt password
+  const comparePassword = await bcrypt.compare(data.password, user.password);
+  // if user password and database password did not match
+  if (!comparePassword) {
+    throw { status: 400, message: "Invalid email or password" };
+  }
+
+  return user;
+};
+
+//update password
+const updatePassword = async (data: IUser, id: any) => {
+  //1. check user from id
+  const user = await User.findOne({ where: { id: id } });
+  // 2. if user not found
+  if (!user) {
+    throw { status: 404, message: "User not found" };
+  }
+  // 3. compare currentPassword from user password
+  const isMatch = await bcrypt.compare(data.currentpassword, user.password);
+  // 4. if currenPassword do not match  with user password
+  if (!isMatch) {
+    throw { status: 401, message: "Invalid Password" };
+  }
+  // 5. hash the new password 
+  const hashPassword = await bcrypt.hash(data.password, 12);
+  // 6. Save the new hasspassword 
+  const update = await User.update(
+    {
+      password: hashPassword,
+    },
+    { where: { id } }
+  );
+
+  // 7. return success
+  return {update, message: "Password Update Successfully"};
+};
+export default { authRegister, authLogin, updatePassword };
